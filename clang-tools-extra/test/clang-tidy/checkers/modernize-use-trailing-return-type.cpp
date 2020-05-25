7187f87651ffc21d3be1f37c18b820df7e455e21
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-05-25 09:52:16 +0200

    fixed unit tests

--- clang-tools-extra/test/clang-tidy/checkers/modernize-use-trailing-return-type.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize-use-trailing-return-type.cpp
@@ -557,11 +557,11 @@ concept floating_point = std::is_same<T, float>::value || std::is_same<T, double
 
 std::floating_point auto con1();
 // CHECK-MESSAGES: :[[@LINE-1]]:26: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto con1() -> std::floating_point auto;{{{$}}
+// CHECK-FIXES: {{^}}auto con1() -> std::floating_point auto;{{$}}
 
 std::floating_point auto con1() { return 3.14f; }
 // CHECK-MESSAGES: :[[@LINE-1]]:26: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto con1() -> std::floating_point auto { return 3.14f; };{{{$}}
+// CHECK-FIXES: {{^}}auto con1() -> std::floating_point auto { return 3.14f; }{{$}}
 
 namespace a {
 template <typename T>
@@ -573,25 +573,25 @@ concept BinaryConcept = true;
 
 a::Concept decltype(auto) con2();
 // CHECK-MESSAGES: :[[@LINE-1]]:27: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto con2() -> a::Concept decltype(auto);{{{$}}
+// CHECK-FIXES: {{^}}auto con2() -> a::Concept decltype(auto);{{$}}
 
 a::BinaryConcept<int> decltype(auto) con3();
-// CHECK-MESSAGES: :[[@LINE-1]]:27: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto con3() -> a::BinaryConcept<int> decltype(auto);{{{$}}
+// CHECK-MESSAGES: :[[@LINE-1]]:38: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
+// CHECK-FIXES: {{^}}auto con3() -> a::BinaryConcept<int> decltype(auto);{{$}}
 
 const std::floating_point auto* volatile con4();
 // CHECK-MESSAGES: :[[@LINE-1]]:42: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto con4() -> const std::floating_point auto* volatile;{{{$}}
+// CHECK-FIXES: {{^}}auto con4() -> const std::floating_point auto* volatile;{{$}}
 
 template <typename T>
 int req1(T t) requires std::floating_point<T>;
 // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-// CHECK-FIXES: {{^}}auto req1(T t) -> int requires std::floating_point<T>;{{{$}}
+// CHECK-FIXES: {{^}}auto req1(T t) -> int requires std::floating_point<T>;{{$}}
 
 template <typename T>
 T req2(T t) requires requires { t + t; };
-  // CHECK-MESSAGES: :[[@LINE-1]]:5: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
-  // CHECK-FIXES: {{^}}auto req2(T t) -> T requires requires { t + t; };{{{$}}
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: use a trailing return type for this function [modernize-use-trailing-return-type]
+  // CHECK-FIXES: {{^}}auto req2(T t) -> T requires requires { t + t; };{{$}}
 
 #endif
 
